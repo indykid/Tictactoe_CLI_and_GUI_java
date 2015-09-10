1e3c206655ca849b026cb5e7c83794bad5eb779e
jarkyn <jarkyn@ymail.com>
2015-09-10 22:11:41 +0100

    initial tests for Board

--- production/source/kg/jarkyn/Board.java
+++ production/source/kg/jarkyn/Board.java
@@ -1,1 +1,58 @@
 package kg.jarkyn;
+
+import java.util.Arrays;
+
+public class Board {
+    private static final int DEFAULT_SIZE = 3;
+    public int size = DEFAULT_SIZE;
+    public String[] moves = new String[9];
+
+    public Board() {
+    }
+
+    public Board(String[] moves) {
+        this.moves = moves;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Board board = (Board) o;
+
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        return Arrays.equals(moves, board.moves);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(moves);
+    }
+
+    public boolean isEmpty() {
+        return isAllEqualAndNull(moves);
+    }
+
+    public Board addMove(int position, String mark) {
+        String[] newMoves = moves.clone();
+        newMoves[position] = mark;
+        return new Board(newMoves);
+    }
+
+    public String markAt(int position) {
+        return moves[position];
+    }
+
+    private boolean isAllEqualAndNull(String[] moves) {
+        return moves[0] == null && isAllEqual(moves);
+    }
+
+    private boolean isAllEqual(String[] moves) {
+        for (int move = 1; move < moves.length; move++) {
+            if (moves[0] != moves[move]) return false;
+        }
+        return true;
+    }
+}
--- test/source/kg/jarkyn/BoardTest.java
+++ test/source/kg/jarkyn/BoardTest.java
@@ -4,4 +4,47 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest {
+    @Test
+    public void hasDefaultSize() {
+        assertEquals(3, new Board().size);
+    }
+
+    @Test
+    public void emptyAtTheStartByDefault() {
+        assertTrue(new Board().isEmpty());
+    }
+
+    @Test
+    public void canHaveMovesFromTheStart() {
+        String[] moves = new String[9];
+        moves[0] = "x";
+
+        assertFalse(new Board(moves).isEmpty());
+
+    }
+
+    @Test
+    public void knowsMarkForGivenPosition() {
+        String[] moves = new String[9];
+        moves[0] = "x";
+
+        assertEquals("x", new Board(moves).markAt(0));
+    }
+
+    @Test
+    public void twoBoardsAreEqualIfHaveSameMoves() {
+        String[] moves = new String[9];
+        moves[0] = "x";
+
+        assertEquals(new Board(moves), new Board().addMove(0, "x"));
+    }
+
+    @Test
+    public void addingMoveReturnsNewBoardWithTheMoveAdded() {
+        Board newBoard = new Board().addMove(0, "x");
+        String[] moves = new String[9];
+        moves[0] = "x";
+
+        assertEquals(new Board(moves), newBoard);
+    }
 }
