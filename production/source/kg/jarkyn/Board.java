44fc484062205f558b56b653451cbb7e0cc17aa1
jarkyn <jarkyn@ymail.com>
2015-09-14 02:17:55 +0100

    implements winning rules in Board

--- production/source/kg/jarkyn/Board.java
+++ production/source/kg/jarkyn/Board.java
@@ -5,20 +5,27 @@ import java.util.Arrays;
 
 public class Board {
     private static final int DEFAULT_SIZE = 3;
+    private static final int[][] WINNING_POSITIONS = new int[][]{
+        {0, 1, 2},
+        {3, 4, 5},
+        {6, 7, 8},
+        {0, 3, 6},
+        {1, 4, 7},
+        {2, 5, 8},
+        {0, 4, 8},
+        {2, 4, 6}
+    };
+
     public int size;
     public String[] moves;
     public ArrayList<Integer> available;
 
     public Board() {
-        this(new String[9], DEFAULT_SIZE);
+        this(new String[9]);
     }
 
     public Board(String[] moves) {
-        this(moves, DEFAULT_SIZE);
-    }
-
-    public Board(String[] moves, int size) {
-        this.size = size;
+        this.size = DEFAULT_SIZE;
         this.moves = moves;
         this.available = setAvailable();
     }
@@ -53,7 +60,7 @@ public class Board {
     }
 
     private ArrayList<Integer> setAvailable() {
-        ArrayList<Integer> available = new ArrayList<Integer>();
+        ArrayList<Integer> available = new ArrayList<>();
         for (int position = 0; position < moves.length; position++) {
             if (isEmptyPosition(position)) {
                 available.add(position);
@@ -64,5 +71,35 @@ public class Board {
 
     private boolean isEmptyPosition(int position) {
         return markAt(position) == null;
+    }
+
+    public boolean isFull() {
+        return available.size() == 0;
+    }
+
+    public int[] winLine() {
+        for (int[] line : WINNING_POSITIONS) {
+            if (isOccupied(line[0]) && isSameMark(line)) {
+                return line;
+            }
+        }
+        return new int[0];
+    }
+
+    private boolean isOccupied(int position) {
+        return moves[position] != null;
+    }
+
+    private boolean isSameMark(int[] line) {
+        for (int index = 0; index < line.length - 1; index++) {
+            if (!moves[line[index]].equals(moves[line[index + 1]])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String winnerMark() {
+        return moves[winLine()[0]];
     }
 }
--- test/source/kg/jarkyn/BoardTest.java
+++ test/source/kg/jarkyn/BoardTest.java
@@ -16,14 +16,14 @@ public class BoardTest {
     @Test
     public void knowsAvailablePositionsWhenNoMovesMade() {
         Board board = new Board();
-        ArrayList<Integer> expectedAvailable = new ArrayList(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
+        ArrayList<Integer> expectedAvailable = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
 
         assertEquals(expectedAvailable, board.available);
     }
 
     @Test
     public void knowsAvailablePositionsWhenMovesWereMade() {
-        ArrayList<Integer> expectedAvailable = new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
+        ArrayList<Integer> expectedAvailable = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
         String[] moves = new String[9];
         moves[0] = "x";
 
@@ -56,7 +56,7 @@ public class BoardTest {
     }
 
     @Test
-    public void knowsIfMoveIsValid() {
+    public void knowsVacantMoveIsValid() {
         Board board = new Board();
 
         assertTrue(board.isValidMove(0));
@@ -77,5 +77,76 @@ public class BoardTest {
         Board board = new Board();
 
         assertFalse(board.isValidMove(10));
+    }
+
+    @Test
+    public void knowsWhenNotFull() {
+        Board board = new Board();
+
+        assertFalse(board.isFull());
+    }
+
+    @Test
+    public void knowsWhenFull() {
+        String[] moves = {"x", "o", "x",
+                          "x", "x", "o",
+                          "o", "o", "x"};
+        Board board = new Board(moves);
+
+        assertTrue(board.isFull());
+    }
+
+    @Test
+    public void returnsWinningRow() {
+        String[] moves = {"x", "x", "x",
+                          null, "o", "o",
+                          null, null, null};
+        Board board = new Board(moves);
+        int[] expectedWinLine = {0, 1, 2};
+
+        assertArrayEquals(expectedWinLine, board.winLine());
+    }
+
+    @Test
+    public void returnsWinningColumn() {
+        String[] moves = {"x", null, null,
+                          "x", "o", "o",
+                          "x", null, null};
+        Board board = new Board(moves);
+        int[] expectedWinLine = {0, 3, 6};
+
+        assertArrayEquals(expectedWinLine, board.winLine());
+    }
+
+    @Test
+    public void returnsWinningDiagonal() {
+        String[] moves = {"x", "o", "o",
+                          null, "x", null,
+                          null, null, "x"};
+        Board board = new Board(moves);
+        int[] expectedWinLine = {0, 4, 8};
+
+        assertArrayEquals(expectedWinLine, board.winLine());
+    }
+
+    @Test
+    public void returnsNoWinningLine() {
+        String[] moves = {"x", "o", "o",
+                          "o", "x", "x",
+                          "o", "x", "o"};
+        Board board = new Board(moves);
+        int[] expectedWinLine = new int[0];
+
+        assertArrayEquals(expectedWinLine, board.winLine());
+    }
+
+    @Test
+    public void returnsWinnerMark() {
+        String[] moves = {"x", "x", "x",
+                          null, "o", "o",
+                          null, null, null};
+        Board board = new Board(moves);
+
+        assertEquals("x", board.winnerMark());
     }
 }
