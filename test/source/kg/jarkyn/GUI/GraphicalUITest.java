ef3f4b52b205b15db0fa1a0111b44d1f8d535909
jarkyn <jarkyn@ymail.com>
2015-10-26 18:22:49 +0000

    refactors test

--- test/source/kg/jarkyn/GUI/GraphicalUITest.java
+++ test/source/kg/jarkyn/GUI/GraphicalUITest.java
@@ -4,34 +4,30 @@ import javafx.embed.swing.JFXPanel;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import kg.jarkyn.Core.GameOption;
-import kg.jarkyn.GUI.ViewComponents.GameSelectionButton;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class GraphicalUITest {
+
     @Test
     public void displaysGameSelector() {
-        new JFXPanel();
+        setupJFXEnvironment();
         Scene scene = new Scene(new StackPane());
         GraphicalUI ui = new GraphicalUI(scene);
 
         ui.displayGameSelector();
 
-        List<Node> controls = getChildren(scene);
-        assertEquals(3, controls.size());
-        assertThat(((GameSelectionButton) controls.get(0)).getGameOption(), is(GameOption.AI_FIRST));
-        assertThat(((GameSelectionButton) controls.get(1)).getGameOption(), is(GameOption.AI_SECOND));
-        assertThat(((GameSelectionButton) controls.get(2)).getGameOption(), is(GameOption.HUMAN_ONLY));
+        assertEquals(3, getChildren(scene).size());
+    }
+
+    private void setupJFXEnvironment() {
+        new JFXPanel();
     }
 
     private List<Node> getChildren(Scene scene) {
         return scene.getRoot().getChildrenUnmodifiable();
     }
-
 }
